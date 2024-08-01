0bb5560f36058b019cfd773e2678a20203e0b09a
szho0086 <szho0086@student.monash.edu>
2024-08-01 13:24:14 +1000

    The lab exercises in week1 and week2

---
+++ Week1/fit5032-week1-firstVueProject/src/components/MyName.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Congrats!</h1>
+    <h3>Song Zhou</h3>
+    <p><a href="https://github.com/Nicolsonsz" target="_blank">https://github.com/Nicolsonsz</a></p>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  color: yellow;
+  font-size: 2.6rem;
+  text-align: center;
+}
+
+h3 {
+  color: white;
+  font-size: 1.2rem;
+  text-align: center;
+}
+
+p {
+  text-align: center;
+}
+
+</style>
